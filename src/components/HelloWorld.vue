6627b33dc91dcefe24b501c99952848e73caa054
Gustavo Leindecker Pereira <gustalp@gmail.com>
2021-03-14 16:03:23 -0300

    Changing templates from html to pug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,28 @@
-<template>
-  <h1>{{ msg }}</h1>
+<template lang="pug">
+h1 {{ msg }}
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank">Vetur</a>
-    or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    (if using
-    <code>&lt;script setup&gt;</code>)
-  </p>
+p
+  span Recommended IDE setup:
+  a(href="https://code.visualstudio.com/" target="_blank") VSCode + 
+  a(href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank") Vetur or 
+  a(href="https://github.com/johnsoncodehk/volar" target="_blank") Volar (if using 
+  code &lt;script setup&gt;
+  span )
 
-  <p>See <code>README.md</code> for more information.</p>
+p
+  span See 
+  code README.md 
+  span for more information.
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
+p
+  a(href="https://vitejs.dev/guide/features.html" target="_blank") Vite Docs | 
+  a(href="https://v3.vuejs.org/" target="_blank") Vue 3 Docs
 
-  <button @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+button(@click="count++") count is: {{ count }}
+p
+  span Edit 
+  code components/HelloWorld.vue 
+  span to test hot module replacement.
 </template>
 
 <script lang="ts">
